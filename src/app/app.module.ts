ab2262f1fddd508afcdfe9c4609dba18b7fd3600
Emmanuel Owolabi <owoemma59@gmail.com>
2023-11-18 13:25:10 +0100

    added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HeroComponent],
+  declarations: [AppComponent, NavbarComponent, HeroComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
